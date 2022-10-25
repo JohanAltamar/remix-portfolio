f98b6480287e0c4e429043391b5a9712076c7a86
Johan Altamar <johan.altamar.dev@gmail.com>
2022-10-24 21:24:20 -0500

    error messaged added to form field

--- app/components/form-field.tsx
+++ app/components/form-field.tsx
@@ -1,10 +1,12 @@
-// app/components/form-field.tsx
+import { useEffect, useState } from "react";
+
 interface FormFieldProps {
   htmlFor: string;
   label: string;
-  type?: string;
   value: any;
+  error?: string;
   onChange?: (...args: any) => any;
+  type?: string;
 }
 
 export function FormField({
@@ -12,21 +14,34 @@ export function FormField({
   label,
   type = "text",
   value,
+  error = "",
   onChange = () => {},
 }: FormFieldProps) {
+  const [errorText, setErrorText] = useState(error);
+
+  useEffect(() => {
+    setErrorText(error);
+  }, [error]);
+
   return (
     <>
       <label htmlFor={htmlFor} className="text-blue-600 font-semibold">
         {label}
       </label>
       <input
-        onChange={onChange}
+        onChange={(e) => {
+          onChange(e);
+          setErrorText("");
+        }}
         type={type}
         id={htmlFor}
         name={htmlFor}
         className="w-full p-2 rounded-xl my-2"
         value={value}
       />
+      <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
+        {errorText || ""}
+      </div>
     </>
   );
 }
